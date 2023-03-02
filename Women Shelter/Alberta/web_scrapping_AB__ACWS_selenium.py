637f17f55f6716e4c2c77cfd976c29b6451c7325
Plant Power <cainveg@gmail.com>
2023-03-01 22:08:59 -0800

    add final touch for shelter safe, simplify phone no

--- Women Shelter/Alberta/web_scrapping_AB__ACWS_selenium.py
+++ Women Shelter/Alberta/web_scrapping_AB__ACWS_selenium.py
@@ -5,7 +5,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
 
 options = Options()
