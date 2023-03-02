00143cbe51808c29b4219d653e452f8044f44d34
Plant Power <cainveg@gmail.com>
2023-03-01 19:08:24 -0800

    scrap ACWS site

--- Women Shelter/Alberta/web_scrapping_MB_selenium.py
+++ Women Shelter/Alberta/web_scrapping_MB_selenium.py
@@ -1,7 +1,6 @@
 # author: Minh
-# date: Feb 24 2023
+# date: Mar 01 2023
 
-import re
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,63 +12,31 @@ options = Options()
 options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=options, executable_path="chromedriver")
 
-df = pd.read_csv("/Users/jedi/Desktop/input.csv")
-city = df.city.values.tolist()
-name = df.name.values.tolist()
-
-address_1 = []
-address_2 = []
-phone_1 = []
-phone_2 = []
+name = []
+region = []
+business_phone = []
+emergency_phone = []
+website = []
 link = []
 
-for i in range(len(name)):
-    gName = "+".join(name[i].split(" ") + city[i].split(" "))
-    temp_link = f"https://www.google.com/search?q={gName}"
-    print(name[i], city[i])
-    print(temp_link)
+driver.get("https://acws.ca/shelters/")
+time.sleep(3)
+
+for temp_name in driver.find_elements(By.CLASS_NAME, "font-purple"):
+    print(temp_name.text)
+    name.append(temp_name)
+
+name = name[2:] # remove first 2 unwanted name in the list
+
+for temp_region in driver.find_elements(By.CLASS_NAME, "font-light-purple"):
+    print(temp_region.text)
+    region.append(temp_region)
+
+for temp_link in driver.find_elements(By.XPATH, '//*[@id="search-results"]/div/div/div/div/div[3]/div[1]/h4/a'):
+    print(temp_link.get_attribute("href"))
     link.append(temp_link)
 
-    driver.get(temp_link)
-    time.sleep(2)
-
-    try:
-        temp_address = driver.find_element(By.XPATH, '//*[@id="kp-wp-tab-overview"]/div[1]/div/div/div/div/div/div[4]/div/div/div/span[2]')
-        print(temp_address.text)
-        address_1.append(temp_address.text)
-    except NoSuchElementException:
-        print("No Address 1")
-        address_1.append("No Address 1")
-        pass
-
-    try:
-        temp_address = driver.find_element(By.XPATH, '//*[@id="kp-wp-tab-overview"]/div[1]/div/div/div/div/div/div[5]/div/div/div/span[2]')
-        print(temp_address.text)
-        address_2.append(temp_address.text)
-    except NoSuchElementException:
-        print("No Address 2")
-        address_2.append("No Address 2")
-        pass
-
-    try:
-        temp_phone = driver.find_element(By.XPATH, '//*[@id="kp-wp-tab-overview"]/div[1]/div/div/div/div/div/div[6]/div/div/div/span[2]/span/a/span/span')
-        print(temp_phone.text)
-        phone_1.append(temp_phone.text)
-    except NoSuchElementException:
-        print("No Phone 1")
-        phone_1.append("No Phone 1")
-        pass
-
-    try:
-        temp_phone = driver.find_element(By.XPATH, '//*[@id="kp-wp-tab-overview"]/div[1]/div/div/div/div/div/div[7]/div/div/div/span[2]/span/a/span/span')
-        print(temp_phone.text)
-        phone_2.append(temp_phone.text)
-    except NoSuchElementException:
-        print("No Phone 2")
-        phone_2.append("No Phone 2")
-        pass
-
-new_df = pd.DataFrame(list(zip(link, name, city, address_1, address_2, phone_1, phone_2)), columns=["link", "name", "city", "address 1", "address 2", "phone 1", "phone 2"])
-new_df.to_csv('/Users/jedi/Desktop/sample_data.csv')
+df = pd.DataFrame(list(zip(name, region, link)), columns=["name", "region", "link"])
+df.to_csv('/Users/jedi/Desktop/sample_data.csv')
 
 driver.close()
