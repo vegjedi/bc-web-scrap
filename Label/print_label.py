5a2dbc270b43b9020acf521d84171c187745edfd
Plant Power <cainveg@gmail.com>
2023-03-08 20:49:56 -0800

    printing small labels

--- Label/print_label.py
+++ Label/print_label.py
@@ -2,40 +2,22 @@
 # date: Jan 16 2023
 
 import pandas as pd
-import click
 from blabel import LabelWriter
 
-# df = pd.read_csv("/Users/jedi/Desktop/sample.csv")
-# post = df.post.values.tolist()
+df = pd.read_csv("/Users/jedi/Desktop/input.csv")
 
-# line_1=[]
-# line_2=[]
-# postal = []
-# address = []
+name = df.name.tolist()
+phone = df.phone.tolist()
+line_1 = df.line_1.tolist()
+line_2 = df.line_2.tolist()
+records = []
 
-# for i in range(len(post)):
-#     post[i] = post[i].replace("\n", "\r")
-#     split = post[i].split("\r")
-#     line_1.append(split[0])
-#     line_2.append(split[1])
-#     postal.append(split[1][-7:])
-#     address.append(split[0] + split[1])
+for i in range(len(name)):
+    temp_dict = dict(facility_name = name[i], phone_no = phone[i], add_line_1= line_1[i], add_line_2 = line_2[i])
+    records.append(temp_dict)
 
-# new_df = pd.DataFrame(list(zip(address, line_1, line_2, postal)), columns=["address", "line_1", "line_2", "postal"])
-# new_df.to_csv('/Users/jedi/Desktop/street_data.csv')
+print(records)
 
-@click.command()
-@click.option('-i', '--input',  required=True, type=str, help="CSV file with columns that match the variables in the HTML template")
-@click.option('-o', '--output', default="output.pdf", type=str, help="Name of the output PDF")
-@click.option('-t', '--template', default="item_template.html", type=str, help="Name of the HTML template file", show_default="item_template.html")
-@click.option('-s', '--style', default="style.css", type=str, help="Name of the CSS file", show_default="style.css")
+# label_writer = LabelWriter("item_template.html", default_stylesheets=("style.css",))
 
-def write_labels(input, output, template, style):
-    df = pd.read_csv(input)
-    records = df.to_dict('records')
-
-    label_writer = LabelWriter(template, default_stylesheets=(style,))
-    label_writer.write_labels(records, target=output)     
-
-if __name__ == '__main__':
-    write_labels()   
+# label_writer.write_labels(records, target='/Users/jedi/Desktop/qrcode_and_label.pdf')
