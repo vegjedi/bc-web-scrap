83115ed6776831e911df100c080b8d3ad0626bd4
Plant Power <cainveg@gmail.com>
2023-03-11 19:24:57 -0800

    upper case and remove .

--- Label/print_label.py
+++ Label/print_label.py
@@ -7,13 +7,14 @@ from blabel import LabelWriter
 df = pd.read_csv("/Users/jedi/Desktop/input.csv")
 
 name = df.name.tolist()
-phone = df.phone.tolist()
 line_1 = df.line_1.tolist()
 line_2 = df.line_2.tolist()
 records = []
+name_1 = []
+name_2 = []
 
 for i in range(len(name)):
-    temp_dict = dict(facility_name = name[i], phone_no = phone[i], add_line_1= line_1[i], add_line_2 = line_2[i])
+    temp_dict = dict(facility_name = (name[i].upper()).replace(".",""), add_line_1= (line_1[i].upper()).replace(".", ""), add_line_2 = line_2[i].upper())
     records.append(temp_dict)
 
 label_writer = LabelWriter("item_template.html", default_stylesheets=("style.css",))
